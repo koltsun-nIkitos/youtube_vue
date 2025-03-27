4181155fcbc4e0fe3ef8bb4d22f249fe3a378e99
Koltsun Nikita <koltsun-nv@yandex.ru>
2025-03-28 00:08:06 +0300

    v1.8 Slice MobileSidebar. Add animation to close mobile sidebar

---
+++ src/components/SidebarContent.vue
@@ -0,0 +1,86 @@
+<template>
+  <SidebarSection>
+    <ul>
+      <SidebarItem label="Home" icon="home" isActive />
+      <SidebarItem label="Trending" icon="fire" />
+      <SidebarItem label="Subscriptions" icon="collection" />
+    </ul>
+  </SidebarSection>
+
+  <SidebarSection>
+    <ul>
+      <SidebarItem label="Library" icon="duplicate" />
+      <SidebarItem label="History" icon="clock" />
+    </ul>
+  </SidebarSection>
+
+  <SidebarSection class="px-8 py-4 border-b font-medium leading-5">
+    <span class="text-sm">
+      Sign in to like videos, comment, and subscribe
+    </span>
+    <ButtonLogin
+      class="focus:outline-none flex items-center px-2 py-0.5 mt-3 text-sm text-blue-700 uppercase border border-blue-700 rounded-sm"
+    />
+  </SidebarSection>
+
+  <SidebarSection>
+    <SidebarSectionHeading title="best of YouTube" />
+
+    <ul>
+      <SidebarItem label="Music" icon="musicNote" />
+      <SidebarItem label="Sports" icon="speakerphone" />
+      <SidebarItem label="Gaming" icon="puzzle" />
+      <SidebarItem label="Movies" icon="film" />
+      <SidebarItem label="News" icon="newspaper" />
+      <SidebarItem label="Live" icon="statusOnline" />
+      <SidebarItem label="360&deg; Video" icon="eye" />
+    </ul>
+  </SidebarSection>
+
+  <SidebarSection>
+    <ul>
+      <SidebarItem label="Browse channels" icon="plusCircle" />
+    </ul>
+  </SidebarSection>
+
+  <SidebarSection>
+    <sidebarSectionHeading title="more from youtube" />
+
+    <ul>
+      <SidebarItem label="YouTube Premium" icon="play" />
+      <SidebarItem label="Live" icon="statusOnline" />
+    </ul>
+  </SidebarSection>
+
+  <SidebarSection>
+    <ul>
+      <SidebarItem label="Settings" icon="cog" />
+      <SidebarItem label="Report history" icon="flag" />
+      <SidebarItem label="Help" icon="questionMarkCircle" />
+      <SidebarItem label="Send feedback" icon="annotation" />
+    </ul>
+  </SidebarSection>
+
+  <SidebarSection class="px-6 py-4 text-xs font-semibold space-y-4">
+    <TheFooter />
+  </SidebarSection>
+</template>
+
+<script>
+  import SidebarSection from './SidebarSection.vue';
+  import SidebarSectionHeading from './SidebarSectionHeading.vue';
+  import SidebarItem from './SidebarItem.vue';
+  import ButtonLogin from './ButtonLogin.vue';
+  import TheFooter from './TheFooter.vue';
+
+  export default{
+    components: {
+      SidebarSection,
+      SidebarSectionHeading,
+      SidebarItem,
+      ButtonLogin,
+      TheFooter,
+
+    }
+  }
+</script>
---
+++ src/components/TheSidebarMobileOverlay.vue
@@ -0,0 +1,7 @@
+<template>
+  <!-- hidden -->
+  <div class="fixed inset-0 bg-black bg-opacity-60 z-30">
+  </div>
+</template>
+
+
