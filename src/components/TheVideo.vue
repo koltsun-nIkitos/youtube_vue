da58df159e0b74a15f9552904816a77ff3f79c66
Koltsun Nikita <koltsun-nv@yandex.ru>
2025-04-01 22:14:03 +0300

    v1.15 added some logic to open dropdown of vertical dots

--- src/components/TheVideo.vue
+++ src/components/TheVideo.vue
@@ -1,5 +1,5 @@
 <template>
-  <a href="#" class="mb-7 group">
+  <a  class="mb-7 group">
     <VideoThumbnail :index="index"/>
     
     <VideoInfo :index="index"/>
--- src/components/VideoDropdown.vue
+++ src/components/VideoDropdown.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="relative ml-auto -mt-1">
     <button
-      @click="isOpen = !isOpen"
-      class="ml-auto -mt-1 p-1 opacity-0 group-hover:opacity-100 text-gray-500 hover:text-gray-700 focus:outline-none"
+      @click="toggle"
+      :class="buttonClasses"
     >
       <BaseIcon class="w-6 h-6" name="dotsVertical" />
       
@@ -19,7 +19,7 @@
             ref="dropdown"
             @keydown.esc="isOpen = false" 
             tabindex="-1"
-            class="absolute top-9 -right-full sm:right-0 bg-white w-48 rounded shadow focus:outline-none"
+            :class="dropDownClasses"
           >
             <section class="py-2">
               <ul>
@@ -38,6 +38,7 @@
 </template>
 
 <script>
+import { ref } from "vue";
 import BaseIcon from "./BaseIcon.vue";
 import VideoDropdownListItem from "./VideoDropdownListItem.vue";
 
@@ -50,6 +51,7 @@ export default {
   data() {
     return {
       isOpen : false,
+      positionClasses: []
     }
   },
 
@@ -69,6 +71,117 @@ export default {
         this.isOpen = false
       }
     })
+  },
+
+  computed:{
+    buttonClasses(){
+      return [
+        'ml-auto', 
+        '-mt-1', 
+        'p-1', 
+        'opacity-0', 
+        'group-hover:opacity-100', 
+        'text-gray-500', 
+        'hover:text-gray-700', 
+        'focus:outline-none',
+      ]
+    },
+
+    dropDownClasses(){
+      return[
+        'z-10',
+        'absolute', 
+        // 'top-9', 
+        // '-right-full', 
+        // 'sm:right-0', 
+        'bg-white', 
+        'w-48', 
+        'rounded', 
+        'shadow', 
+        'focus:outline-none',
+        ...this.positionClasses,
+      ]
+    }
+  },
+
+  methods:{
+    toggle(event){
+      this.isOpen = !this.isOpen
+
+      if(this.isOpen){
+        this.$nextTick(() =>{
+          this.positionClasses = this.getPositionClasses(event)
+        })
+      }
+    },
+
+    getPositionClasses(event){
+      return [
+        this.getTopClass(event),
+        this.getRightClass(event),
+        this.getLeftClass(event),
+      ]
+    },
+
+    getTopClass(event){
+      const clickCoordY = event.clientY
+      const buttonHeight = event.currentTarget.offsetHeight
+      const dropdownHeight = this.$refs.dropdown.offsetHeight
+
+      if (window.innerHeight - clickCoordY < dropdownHeight){
+        return '-top-14'
+      }
+
+      if (window.innerHeight - clickCoordY < dropdownHeight + buttonHeight){
+        return 'top-0'
+      }
+
+      return 'top-9'
+    },
+
+    getRightClass(event){
+      const clickCoordX = event.clientX
+      const clickCoordY = event.clientY
+      const buttonHeight = event.currentTarget.offsetHeight
+      const dropdownWidth = this.$refs.dropdown.offsetWidth
+      const dropdownHeight = this.$refs.dropdown.offsetHeight
+
+      if (window.innerWidth - clickCoordX > dropdownWidth){
+        return 'right-auto'
+      }
+
+      if (window.innerHeight - clickCoordY > dropdownHeight + buttonHeight){
+        return 'right-0'
+      }
+
+      if(window.innerHeight - clickCoordY > dropdownHeight){
+        return 'right-8'
+      }
+
+      return 'right-0'
+    },
+
+    getLeftClass(event){
+      const clickCoordX = event.clientX
+      const clickCoordY = event.clientY
+      const buttonHeight = event.currentTarget.offsetHeight
+      const dropdownWidth = this.$refs.dropdown.offsetWidth
+      const dropdownHeight = this.$refs.dropdown.offsetHeight
+
+      if (window.innerWidth - clickCoordX < dropdownWidth){
+        return 'right-auto'
+      }
+
+      if (window.innerHeight - clickCoordY < dropdownHeight){
+        return 'left-auto'
+      }
+
+      if(window.innerHeight - clickCoordY > dropdownHeight + buttonHeight){
+        return ' left-auto'
+      }
+
+      return 'left-8'
+    },
   },
 };
 </script>
